47c525a63ee34fa88a4c64f18b093518cbd95cde
Huseyn <hsbziyev@gmail.com>
2025-11-29 23:42:07 +0400

    Initial Project

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='w-full h-screen flex justify-center items-center'>App
+        <p>dfdf</p></div>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam pariatur, officia eaque, provident commodi deleniti sint voluptate, minima reiciendis eligendi nesciunt molestias recusandae quasi consequatur iure qui. Ipsam nihil ipsa ad et similique iusto officia eveniet est! Eveniet veritatis, cumque quam illum distinctio nesciunt ipsam, laudantium facilis ab architecto facere.</p>
+    </>
+  )
+}
+
+export default App
