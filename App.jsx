b8331beaf83b2ab09a60bf3391d82832c7881158
Huseyn <hsbziyev@gmail.com>
2025-11-30 09:58:11 +0400

    Secondd

--- App.jsx
+++ App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
+import Navigator from './components/Navigator'
 
 const App = () => {
   return (
-    <>
-      <div className='w-full h-screen flex justify-center items-center'>App
-        <p>dfdf</p></div>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam pariatur, officia eaque, provident commodi deleniti sint voluptate, minima reiciendis eligendi nesciunt molestias recusandae quasi consequatur iure qui. Ipsam nihil ipsa ad et similique iusto officia eveniet est! Eveniet veritatis, cumque quam illum distinctio nesciunt ipsam, laudantium facilis ab architecto facere.</p>
-    </>
+    { Navigator }
   )
 }
 
